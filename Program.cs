ebc867042d036e9c5ca2212eabc9d9ad0d0ae37d
sira72 <120383642+sira72@users.noreply.github.com>
2023-01-03 19:41:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// задача 64. Задайте значение М и N. Напишитепрограмму, которая выведетвсе натуральные числа от M до  N.
+// M=1; N=5. -> " " 1,2,3,4,5" "
+// M=4; N=8. -> " "4,6,7,8" "
+
+object NaturalNumbers(int n, int m)
+{
+    if (n<=m) return $"{n} + NaturalNumbers (n+1,m)";
+    else return String.Empty ;
+}
